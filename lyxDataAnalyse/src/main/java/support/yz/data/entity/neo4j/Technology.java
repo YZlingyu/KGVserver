1999440d6b9a8a4b325cc1d1cd6273d952070d1e
liang930908 <41559934+liang930908@users.noreply.github.com>
2018-07-30 10:19:27 +0800

    Add files via upload

--- lyxDataAnalyse/src/main/java/support/yz/data/entity/neo4j/Technology.java
+++ lyxDataAnalyse/src/main/java/support/yz/data/entity/neo4j/Technology.java
@@ -1,1 +1,23 @@
+package support.yz.data.entity.neo4j;
 
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @Author: yangzhuo
+ * @Description: 技术关键字
+ * @Date: 2018/7/25
+ */
+
+@Setter
+@Getter
+@NodeEntity(label="技术关键字")
+public class Technology {
+	@GraphId
+    private Long id;
+	private String techno;//技术关键字编号
+	private String name;//技术关键字名称
+}
