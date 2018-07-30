d1638f1972c03163e584b6351e19b959b5a084c1
liang930908 <41559934+liang930908@users.noreply.github.com>
2018-07-30 10:24:51 +0800

    Add files via upload

--- lyxDataAnalyse/src/main/java/support/yz/data/mvc/service/inter/ChartService.java
+++ lyxDataAnalyse/src/main/java/support/yz/data/mvc/service/inter/ChartService.java
@@ -1,14 +1,43 @@
 package support.yz.data.mvc.service.inter;
 
-import support.yz.data.entity.Chart;
+import java.util.List;
 
-/*
+import support.yz.data.entity.chart.Chart;
+import support.yz.data.entity.node.EnterpriseBaseImport;
+import support.yz.data.entity.node.NewsBaseOriginal;
+
+/**
  * 生成图表服务接口
  */
 public interface ChartService {
 
-    /*
-     * 保存图表方法
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 保存图表信息
+     * @Date: 11:16 2018/7/24
      */
     public boolean saveChart(Chart chart) throws Exception;
+    
+    /**
+     * @Author: yangzhuo
+     * @Descriptor: 获取图表信息
+     * @Date: 13:20 2018/7/24
+     */
+    public Chart getChartByTitle(String title) throws Exception;
+
+    /**
+     * @Author: yangzhuo
+     * @Descriptor: 从mysql查找所有的已经导入到neo4j的新闻
+     * @Date: 15:24 2018/7/25
+     */
+	List<NewsBaseOriginal> findImports() throws Exception;
+
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 查找所有的已经导入到neo4j的企业
+     * @Date: 15:29 2018/7/25
+     */
+	List<EnterpriseBaseImport> findImport() throws Exception;
+
+	NewsBaseOriginal findByNewsId(String news_id) throws Exception;
 }
---
+++ lyxDataAnalyse/src/main/java/support/yz/data/mvc/service/inter/QueryNeo4jService.java
@@ -0,0 +1,81 @@
+package support.yz.data.mvc.service.inter;
+
+import java.util.List;
+import java.util.Set;
+
+import support.yz.data.entity.neo4j.Technology;
+import support.yz.data.entity.node.Group;
+import support.yz.data.entity.node.News;
+import support.yz.data.entity.node.TechnologyChain;
+
+/**
+ * @Author yangzhuo
+ * @Description: 图数据库相关接口
+ * @Date: 2018/7/25
+ * 封装的5个接口：
+ * 1）通过技术关键字查所有子技术关键字
+ * 2）通过技术关键字查企业
+ * 3）通过企业查技术关键字
+ * 4）通过企业查新闻id
+ * 5）通过企业查询所有关联企业
+ */
+public interface QueryNeo4jService {
+	
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 通过技术关键字查所有子技术关键字
+     * @Date: 11:23 2018/7/25
+     */
+	List<Technology> findTechnologyChildrens(String technology) throws Exception;
+	
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 通过技术关键字查企业
+     * @Date: 13:54 2018/7/25
+     */
+	List<Group> findCompanyByTechnology(String technology) throws Exception;
+
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 通过企业查技术关键字
+     * @Date: 14:19 2018/7/25
+     */
+	List<Technology> findTechnologyByCompany(String company) throws Exception;
+
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 通过企业查新闻id
+     * @Date: 14:28 2018/7/25
+     */
+	List<News> findNewsByCompany(String company) throws Exception;
+
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 通过企业查询所有关联企业
+     * @Date: 14:45 2018/7/25
+     */
+	List<Group> findCompanyByCompany(String company) throws Exception;
+
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 返回技术链
+     * @Date: 14:45 2018/7/25
+     */
+	TechnologyChain findTechnologyChain() throws Exception;
+
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 返回technology_name节点及孩子技术列表
+     * @Date: 15:15 2018/7/25
+     */
+	Set<String> findTechnologyChildren(String string) throws Exception;
+
+	/**
+     * @Author: yangzhuo
+     * @Descriptor: 根据技术关键字查找新闻
+     * @Date: 15:40 2018/7/25
+     */
+	Set<News> findNewsByTechnology(String s) throws Exception;
+
+	TechnologyChain findComByTechInTChain(String technologyName) throws Exception;
+}
