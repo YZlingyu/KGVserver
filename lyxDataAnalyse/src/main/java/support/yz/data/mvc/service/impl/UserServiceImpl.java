827bd736fd1b60165493b91c205a9f905553c611
liang930908 <41559934+liang930908@users.noreply.github.com>
2018-09-14 16:10:23 +0800

    Add files via upload

--- lyxDataAnalyse/src/main/java/support/yz/data/mvc/service/impl/UserServiceImpl.java
+++ lyxDataAnalyse/src/main/java/support/yz/data/mvc/service/impl/UserServiceImpl.java
@@ -1,1 +1,22 @@
+package support.yz.data.mvc.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import support.yz.data.mvc.mapper.UserMapper;
+import support.yz.data.mvc.service.inter.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+    private UserMapper userMapper;
+	
+	@Override
+	public boolean getUser(String userName, String password) throws Exception {
+		if(userMapper.getUser(userName, password) != null)
+			return true;
+		return false;
+	}
+
+}
