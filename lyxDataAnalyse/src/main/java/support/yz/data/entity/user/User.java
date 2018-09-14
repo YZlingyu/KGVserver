01370bbbb715ec5fb20dd484eec8b23733bf904a
liang930908 <41559934+liang930908@users.noreply.github.com>
2018-09-14 16:03:31 +0800

    Add files via upload

--- lyxDataAnalyse/src/main/java/support/yz/data/entity/user/User.java
+++ lyxDataAnalyse/src/main/java/support/yz/data/entity/user/User.java
@@ -1,1 +1,12 @@
+package support.yz.data.entity.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class User {
+	private int userId;
+	private String userName;
+	private String password;
+}
