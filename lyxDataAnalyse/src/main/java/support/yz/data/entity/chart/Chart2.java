e969ab6fa14a93bd9e2295a9ea47b520fad4c744
liang930908 <41559934+liang930908@users.noreply.github.com>
2018-09-21 18:33:24 +0800

    Add files via upload

--- lyxDataAnalyse/src/main/java/support/yz/data/entity/chart/Chart2.java
+++ lyxDataAnalyse/src/main/java/support/yz/data/entity/chart/Chart2.java
@@ -1,1 +1,13 @@
+package support.yz.data.entity.chart;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Chart2 {
+	private String id;
+	private String type;
+	private String chartX;
+	private String chartY;
+}
